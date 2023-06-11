180157f3d64727c4207bffed8b236688b2817b7e
alexvutov <alexeyvutov@gmail.com>
2023-06-11 16:50:40 +0300

    New Exercise

---
+++ Functions - Exercise/nxNMatrix.js
@@ -0,0 +1,15 @@
+function nxNMatrix(n) {
+
+    for (let i = 1; i <= n; i++) {
+        let row = '';
+        for (let j = 1; j <= n; j++) {
+            row += `${n} `;
+
+        }
+        console.log(row);
+    }
+
+
+}
+
+nxNMatrix(3);
